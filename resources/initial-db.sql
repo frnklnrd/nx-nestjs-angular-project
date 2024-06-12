30953e378a200ce6a3e1f514cc872cce63b6a02f
Franklin RD <frivero.duharte@gmail.com>
2024-06-12 12:26:00 +0200

    Add initial users db script

---
+++ resources/initial-db.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO "users"
+    ("id", "created_at", "updated_at", "deleted_at", "username", "email", "password", "is_admin", "is_active", "is_blocked")
+  VALUES
+	  ('1ae87467-1234-4c4c-b901-9c611468ad8d', '2024-06-12 12:00:00:00.000000', '2024-06-12 12:00:00:00.000000', NULL, 'admin', 'admin@localhost.com', '', 'true', 'true', 'false');
+INSERT INTO "users"
+    ("id", "created_at", "updated_at", "deleted_at", "username", "email", "password", "is_admin", "is_active", "is_blocked")
+  VALUES
+	  ('629d99b5-abcd-4015-90cc-5021a8a60d47', '2024-06-12 12:00:00:00.000000', '2024-06-12 12:00:00:00.000000', NULL, 'user', 'user@localhost.com', '', 'false', 'true', 'false');
